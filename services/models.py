72edcffb55d14070d2025da04e03d1486d7e7b3f
Pieter DG <pieter.de.geyndt@gmail.com>
2022-02-26 17:44:06 +0100

    v0.6 - Services

--- services/models.py
+++ services/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Services(models.Model):
-    image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
     body = models.TextField(default="Dit is fake bodytext, vervang mij in de admin")
     quotes= models.TextField(default="Dit is fake quotetext, vervang mij in de admin")
 
--- services/views.py
+++ services/views.py
@@ -5,6 +5,6 @@ def allservices(request):
     services = Services.objects
     return render(request,'services/allservices.html',{'services': services})
 
-def servicedetail(request, about_id):
+def servicedetail(request, service_id):
     detailservice = get_object_or_404(Services, pk=service_id)
     return render(request,'services/detail.html', {'service': detailservice})
