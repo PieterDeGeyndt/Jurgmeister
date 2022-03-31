cc813baef5e75191b7b674bb93498ac28624537f
Pieter De Geyndt <pieter.de.geyndt@gmail.com>
2022-03-31 11:14:06 +0200

    v1.9 - Mollie payment API attempt

--- cocktails/models.py
+++ cocktails/models.py
@@ -82,9 +82,9 @@ class BillingAddress(models.Model):
         return self.user.username
 
 class Payment(models.Model):
-    mollie_payment_id = models.Charfield(maw_length=50)
-    user = models.Foreignkey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
-    amount = models.Floatfield()
+    mollie_payment_id = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
