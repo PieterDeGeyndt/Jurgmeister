93431796a7b6e417537b5e688221eb0d37c5dd57
Pieter DG <pieter.de.geyndt@gmail.com>
2022-06-29 20:19:46 +0200

    Updated add_item

--- cocktails/views.py
+++ cocktails/views.py
@@ -64,7 +64,6 @@ def add_to_cart(request, cocktail_id):
         ordered_date = timezone.now()
         order = Order.objects.create(
             user=request.user, start_date=ordered_date)
-        order.items.delete(all)
         order.items.add(item)
         messages.success(request, "1 " + item.title + " werd aan je wagentje toegevoegd.")
         return redirect("allcocktails")
