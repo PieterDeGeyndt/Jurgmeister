d00798553de2dfaf17abaa18f489e200b34f0ff5
Pieter DG <pieter.de.geyndt@gmail.com>
2022-07-02 14:41:45 +0200

    replaced charge with mollie_id

--- cocktails/views.py
+++ cocktails/views.py
@@ -312,7 +312,7 @@ class PaymentView(LoginRequiredMixin, View):
             order_items.update(ordered_timestamp = payment.timestamp)
             for item in order_items:
                 item.save()
-            order.charge = payment.mollie_payment_id
+            order.mollie_id = payment.mollie_payment_id
             order.ordered = True
             order.ordered_date = payment.timestamp
             order.save()
