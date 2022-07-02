223b8a1a25b96dd4543c66de65541307796e53c4
Pieter DG <pieter.de.geyndt@gmail.com>
2022-07-02 13:33:27 +0200

    Charge id in order

--- cocktails/views.py
+++ cocktails/views.py
@@ -311,7 +311,7 @@ class PaymentView(LoginRequiredMixin, View):
             order_items.update(ordered_timestamp = payment.timestamp)
             for item in order_items:
                 item.save()
-            order.charge = payment
+            order.charge = charge.id
             order.ordered = True
             order.ordered_date = payment.timestamp
             order.save()
